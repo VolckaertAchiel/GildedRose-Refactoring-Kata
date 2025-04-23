a792a836d91515ba31b2383702312bc20b484f2c
Achiel Volckaert <achiel.volckaert@synriselabs.com>
2025-04-24 00:50:38 +0200

    refactor: cleanup brie case

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,13 +21,11 @@ export class GildedRose {
     for (const item of this.items) {
       switch (item.name) {
         case 'Aged Brie':
+          item.sellIn --;
           if (item.quality < 50) {
-            item.quality += 1;
-          }
-          item.sellIn = item.sellIn - 1;
-          if (item.sellIn < 0) {
-            if (item.quality < 50) {
-              item.quality += 1;
+            item.quality ++;
+            if (item.sellIn < 0 && item.quality < 50) {
+              item.quality ++;
             }
           }
           break;
