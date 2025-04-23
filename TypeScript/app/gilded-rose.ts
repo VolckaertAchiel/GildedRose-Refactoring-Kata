be4c684fdfdfad3e448409fd2606d04648bed05d
Achiel Volckaert <achiel.volckaert@synriselabs.com>
2025-04-23 23:43:54 +0200

    refactor: translate big ifs to switch case

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,47 +19,66 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
+      switch (item.name) {
+        case 'Aged Brie':
+          if (item.quality < 50) {
+            item.quality += 1;
           }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          break;
+
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          if (item.quality < 50) {
+            item.quality += 1;
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              item.quality += 1;
             }
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              item.quality += 1;
+            }
+            if (item.quality > 50) {
+              item.quality = 50;
             }
           }
-        }
+          break;
+
+        case 'Sulfuras, Hand of Ragnaros':
+          // Legendary item, quality does not change
+          break;
+
+        default:
+          if (item.quality > 0) {
+            item.quality -= 1;
+          }
+          break;
       }
+
+
+
       if (item.name != 'Sulfuras, Hand of Ragnaros') {
         item.sellIn = item.sellIn - 1;
       }
+
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
+        switch (item.name) {
+          case 'Aged Brie':
+            if (item.quality < 50) {
+              item.quality += 1;
             }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+            break;
+
+          case 'Backstage passes to a TAFKAL80ETC concert':
+            item.quality = 0;
+            break;
+
+          case 'Sulfuras, Hand of Ragnaros':
+            // Legendary item, quality does not degrade
+            break;
+
+          default:
+            if (item.quality > 0) {
+              item.quality -= 1;
+            }
+            break;
         }
       }
     }
